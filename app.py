f983bd5e3fc7d1fd9978e3619fdbba52ad03027e
Griffin <gfluet5@gmail.com>
2021-01-27 20:29:05 -0500

    Adding comments and some more html testing

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 
+# Creating Flask object
 app = Flask(__name__)
 
+# Setting app starting index
 @app.route('/')
+# Displays index.html 
 def index():
 	return render_template('index.html')
+@app.route('/test')
+def testPage():
+	return render_template('other.html')
 
+# Log errors to web page
 if __name__ == '__main__':
 	app.run(debug=True)
